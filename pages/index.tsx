77b6149363cfdf8e4e6e00ab99f7e2642cef1f7f
Vidhan Bhatt <me@vidhan.io>
2021-11-27 01:23:03 -0500

    change name

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,59 +6,12 @@ function App() {
   const [gizmos, setGizmos] = useState<IGizmo[]>([]);
 
   useEffect(() => {
-    async function fetchGizmos() {
+    async function handleGetGizmos() {
       const { data } = await getGizmos();
       setGizmos(data.gizmos);
     }
-    fetchGizmos();
+    handleGetGizmos();
   }, []);
-
-  async function handleGetGizmos() {
-    await getGizmos()
-      .then(({ status, data }) => {
-        if (status !== 200) {
-          throw new Error("Error! Gizmos could not be retrieved.");
-        } else {
-          setGizmos(data.gizmos);
-        }
-      })
-      .catch((err) => console.log(err));
-  }
-
-  /*
-  const handleSaveGizmo = (e: React.FormEvent, formData: IGizmo): void => {
-    e.preventDefault();
-    addGizmo(formData)
-      .then(({ status, data }) => {
-        if (status !== 201) {
-          throw new Error("Error! Gizmo not saved");
-        }
-        setGizmos(data.gizmos);
-      })
-      .catch((err) => console.log(err));
-  };
-
-  const handleUpdateGizmo = (gizmo: IGizmo) => {
-    updateGizmo(gizmo)
-      .then(({ status, data }) => {
-        if (status !== 200) {
-          throw new Error("Error! Gizmo not updated");
-        }
-        setGizmos(data.gizmos);
-      })
-      .catch((err) => console.log(err));
-  };
-
-  const handleDeleteGizmo = (resource: number) => {
-    deleteGizmo(resource)
-      .then(({ status, data }) => {
-        if (status !== 200) {
-          throw new Error("Error! Gizmo not deleted");
-        }
-        setGizmos(data.gizmos);
-      })
-      .catch((err) => console.log(err));
-  };*/
 
   return (
     <div className="flex flex-col gap-16 p-16 App">
